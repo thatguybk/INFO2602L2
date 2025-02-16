d0f3d489daeef434f8b1bba3b9b3a23ca7840b95
thatguybk <78873992+thatguybk@users.noreply.github.com>
2025-02-16 23:08:53 +0000

    fixed init

--- wsgi.py
+++ wsgi.py
@@ -4,11 +4,29 @@ from app import app
 from sqlalchemy.exc import IntegrityError
 
 
+
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
   db.drop_all()
   db.init_app(app)
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
+  db.session.add(bob)
+  db.session.commit()
   print(bob)
-  print('database intialized')
+  print('database initialized')
+
+@app.cli.command("get-user", help="Retrieves a User")
+@click.argument('username', default='bob')
+def get_user(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print(f'{username} not found!')
+    return
+  print(bob)
+
+@app.cli.command('get-users')
+def get_users():
+  # gets all objects of a model
+  users = User.query.all()
+  print(users)
